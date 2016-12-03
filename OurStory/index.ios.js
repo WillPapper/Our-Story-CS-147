1b614814d4703ee44ab28ba3d7c7fdff3714d81e
William Papper <contact@papper.me>
2016-12-03 00:56:55 -0800

    Renamed home screen, overrode navigatorStyle

--- OurStory/index.ios.js
+++ OurStory/index.ios.js
@@ -1,17 +1,25 @@
 import { Navigation } from 'react-native-navigation';
 
-import { Home } from './home.ios.js';
+import { HomeScreen } from './HomeScreen.js';
 import { MapStanford } from './map/MapStanford.js';
 
 // this is where you register all of your app's screens
 export function registerScreens() {
-  Navigation.registerComponent('OurStory.Home', () => Home);
+  Navigation.registerComponent('OurStory.Home', () => HomeScreen);
   Navigation.registerComponent('OurStory.MapStanford', () => MapStanford );
+  console.log("Navigation components registered")
 }
 
 registerScreens();
 
+static navigatorStyleDefinition = {
+  drawUnderNavBar: true,
+  navBarTranslucent: true
+};
+
 // start the app
+console.log("LOOGGING NAVIGATION RESULTS")
+console.log(Navigation);
 Navigation.startTabBasedApp({
   tabs: [
     {
@@ -19,14 +27,15 @@ Navigation.startTabBasedApp({
       screen: 'OurStory.Home', // this is a registered name for a screen
       icon: require('./assets/icons/Home.png'),
       selectedIcon: require('./assets/icons/Home_Active.png'), // iOS only
-      title: 'Home'
-    },
+      title: 'Home',
+      navigatorStyle: navigatorStyleDefinition},
     {
       label: 'Map',
       screen: 'OurStory.MapStanford',
       icon: require('./assets/icons/Map.png'),
       selectedIcon: require('./assets/icons/Map_Active.png'), // iOS only
-      title: 'Map'
+      title: 'Map',
+      navigatorStyle: navigatorStyleDefinition,
     }
   ]
 });
