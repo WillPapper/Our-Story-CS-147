66b4ed33cf45186f2f5376a9982c9b60dd65ed2e
Nishtha Bhatia <nishtha@rescomp-16-332327.stanford.edu>
2016-12-08 15:24:46 -0800

    added x to snap info

--- OurStory/screens/ViewVideo.js
+++ OurStory/screens/ViewVideo.js
@@ -62,11 +62,12 @@ export default class ViewVideo extends Component {
              {this.state.showSnapInfo &&
                <View style = {styles.snapInfo}>
                  {/* Close snap info button -- THIS WILL NEED TO BE MOVED TO THE TOP OF THE SCREEN */}
-                 <View style={styles.closeVideoInfo}>
+
                    <TouchableHighlight onPress={() => this.showSnapInfo()} underlayColor="#DDF8F9">
-                     <Image source={require("../assets/icons/X.png")} style={styles.xImage}></Image>
+                     <Image source={require("../assets/icons/X.png")} style={styles.xImageInfo}></Image>
                  </TouchableHighlight>
-                 </View>
+
+
                  <Text style = {styles.snapInfoText}>Tap to Snap</Text>
                  <Image source={require("../assets/icons/Snap_Active.png")}style={styles.snapImageInfo}></Image>
                  <Text style = {styles.snapInfoCaptionText}>Like an audience listening to spoken word, your snaps accumulate and result in a background noise of appreciation.</Text>
@@ -245,11 +246,12 @@ var styles = StyleSheet.create({
     marginLeft: 15,
     marginRight: 35,
   },
-  closeVideoInfo: {
-    top: 0,
-    position: 'relative',
+  xImageInfo: {
+    position: 'absolute',
+    top: 100,
+    bottom: 0,
+    left: 40,
   },
-
   closeVideo: {
 
   }
